410716670f8645e82ff48b4f00c356ee5949b2e6
Sven Milker <sven.milker@brickmakers.de>
2020-10-03 17:55:46 +0200

    added plain music

---
+++ Assets/Systems/VinylMusicSystem/VinylMusicComponent.cs
@@ -0,0 +1,10 @@
+﻿using SystemBase;
+using UnityEngine;
+
+namespace Assets.Systems.VinylMusicSystem
+{
+    public class VinylMusicComponent : GameComponent
+    {
+        public AudioSource VinylMusicSource;
+    }
+}
---
+++ Assets/Systems/VinylMusicSystem/VinylMusicSystem.cs
@@ -0,0 +1,31 @@
+﻿using SystemBase;
+using Systems.GameState.Messages;
+using UniRx;
+using UnityEngine;
+using Utils;
+
+namespace Assets.Systems.VinylMusicSystem
+{
+    [GameSystem]
+    public class VinylMusicSystem : GameSystem<VinylMusicComponent>
+    {
+        public override void Init()
+        {
+            //could be added to IoC
+            IoC.RegisterSingleton(this);
+        }
+
+        public override void Register(VinylMusicComponent comp)
+        {
+            if (!comp) return;
+
+            StartVinylMusic(comp);
+            //MessageBroker.Default.Receive<GameMsgStart>().Subscribe(_ => StartVinylMusic());
+        }
+
+        private static void StartVinylMusic(VinylMusicComponent comp)
+        {
+            comp.VinylMusicSource.Play();
+        }
+    }
+}
