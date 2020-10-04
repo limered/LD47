5f9092b6312b250c39a6a4dbad9249a83d6f564d
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2020-10-04 17:59:54 +0200

    add scratch spawner

---
+++ Assets/Systems/Obsticles/ScratchSpawnSystem.cs
@@ -0,0 +1,38 @@
+﻿using SystemBase;
+using UniRx;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Assets.Systems.Obsticles
+{
+    [GameSystem]
+    public class ScratchSpawnSystem : GameSystem<ScratchSpawnerComponent>
+    {
+        public override void Register(ScratchSpawnerComponent component)
+        {
+            SystemUpdate(component)
+                .Subscribe(TryToSpawn)
+                .AddTo(component);
+        }
+
+        private void TryToSpawn(ScratchSpawnerComponent obj)
+        {
+            var spawnRate = obj.InverseSpawnPropability;
+            var shouldSpawn = (int)(Random.value * spawnRate) % spawnRate == 0;
+
+            if (!shouldSpawn || obj.CurrentScratches.Count >= obj.MaxScratchCount) return;
+
+            var extends = obj.transform.position - obj.SpawnerBox.size * 0.5f;
+
+            var x = Random.value * obj.SpawnerBox.size.x + extends.x;
+            var y = Random.value * obj.SpawnerBox.size.y + extends.y;
+            var position = obj.SpawnerBox.bounds.ClosestPoint(new Vector3(x, y, 0));
+
+            var scratchNr = (int)(Random.value * obj.ScratchPrefabs.Count);
+
+            var scratch = Object.Instantiate(obj.ScratchPrefabs[scratchNr], position, Quaternion.identity, obj.Plate.transform);
+            obj.CurrentScratches.Add(scratch.gameObject);
+        }
+    }
+}
---
+++ Assets/Systems/Obsticles/ScratchSpawnerComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SystemBase;
+using UnityEngine;
+
+namespace Assets.Systems.Obsticles
+{
+    public class ScratchSpawnerComponent : GameComponent
+    {
+        public int InverseSpawnPropability = 1000;
+        public int MaxScratchCount = 2;
+
+        public BoxCollider SpawnerBox;
+        public GameComponent Plate;
+        public List<GameComponent> ScratchPrefabs;
+        public List<GameObject> CurrentScratches = new List<GameObject>();
+    }
+}
