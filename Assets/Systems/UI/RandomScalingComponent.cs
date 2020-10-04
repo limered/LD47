9787f7cddafa102e348889efcf2c6785a2dbc9ec
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2020-10-04 23:59:23 +0200

    add animations to symbols

---
+++ Assets/Systems/UI/RandomScalingComponent.cs
@@ -0,0 +1,30 @@
+﻿using SystemBase;
+using UnityEngine;
+
+namespace Assets.Systems.UI
+{
+    public class RandomScalingComponent : GameComponent
+    {
+        public float WobbleStrengthPercent = 0.1f;
+        public float WobbleSpeed = 20;
+
+        private Vector3 _basicScale;
+
+        protected override void OverwriteStart()
+        {
+            _basicScale = transform.localScale;
+        }
+
+        private void Update()
+        {
+            var animationSpeed = Time.realtimeSinceStartup * WobbleSpeed;
+
+            var wobble = _basicScale * WobbleStrengthPercent;
+
+            var xScaleModifier = Mathf.Sin(animationSpeed) * wobble.x + _basicScale.x;
+            var yScaleModifier = Mathf.Cos(animationSpeed) * wobble.y + _basicScale.y;
+
+            transform.localScale = new Vector3(xScaleModifier, yScaleModifier, _basicScale.z);
+        }
+    }
+}
