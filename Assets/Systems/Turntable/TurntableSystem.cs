3ffa6e3744569a3c2cbe181a69e5ae195d1c7bca
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2020-10-05 01:32:29 +0200

    stop turntable system on game end

--- Assets/Systems/Turntable/TurntableSystem.cs
+++ Assets/Systems/Turntable/TurntableSystem.cs
@@ -1,6 +1,6 @@
-﻿using SystemBase;
-using Assets.Systems.VinylMusicSystem;
+﻿using Assets.Systems.VinylMusicSystem;
 using GameState.States;
+using SystemBase;
 using UniRx;
 using UnityEngine;
 using Utils;
@@ -14,9 +14,8 @@ namespace Assets.Systems.Turntable
         {
             RegisterWaitable(component);
 
-            IoC.Game.GameStateContext.CurrentState
-                .Where(state => state is Running)
-                .Subscribe(_ => RegisterTurntable(component))
+            WaitOn<VinylMusicComponent>()
+                .ThenOnUpdate(vinylMusic => ChangeRotationSpeed(vinylMusic, component))
                 .AddTo(component);
         }
 
@@ -26,55 +25,48 @@ namespace Assets.Systems.Turntable
                 .Where(state => state is Running)
                 .Subscribe(_ => RegisterVinyl(component))
                 .AddTo(component);
+
+            WaitOn<Turntable>()
+                .ThenOnUpdate(turntable => UpdateSpeed(component, turntable))
+                .AddTo(component);
         }
 
         public override void Register(Arm component)
-        {
-            IoC.Game.GameStateContext.CurrentState
-                .Where(state => state is Running)
-                .Subscribe(_ => RegisterArm(component))
-                .AddTo(component);
-        }
-
-        public override void Register(VinylMusicComponent component) => RegisterWaitable(component);
-
-        private void RegisterTurntable(Turntable component)
-        {
-            WaitOn<VinylMusicComponent>()
-                .ThenOnUpdate(vinylMusic => ChangeRotationSpeed(vinylMusic, component))
-                .AddTo(component);
-        }
-
-        private void RegisterVinyl(Vinyl component)
-        {
-            WaitOn<Turntable>()
-                .ThenOnUpdate(turntable => component
-                    .transform
-                    .Rotate(component.Axis, Time.deltaTime * -turntable.Speed.Value, Space.Self))
-                .AddTo(component);
-
-            StartVinylAnimation(component);
-        }
-
-        private static void StartVinylAnimation(Vinyl component)
-        {
-            component.VinylAnimationGameObject.GetComponent<Animator>().enabled = true;
-        }
-
-        private void RegisterArm(Arm component)
         {
             WaitOn<VinylMusicComponent>()
                 .ThenOnUpdate(vinylMusicComponent => UpdateNeedlePosition(component, vinylMusicComponent))
                 .AddTo(component);
         }
 
+        public override void Register(VinylMusicComponent component) => RegisterWaitable(component);
+
+        private static void UpdateSpeed(Vinyl component, Turntable turntable)
+        {
+            if (!(IoC.Game.GameStateContext.CurrentState.Value is Running)) return;
+
+            component.transform
+                .Rotate(component.Axis, Time.deltaTime * -turntable.Speed.Value, Space.Self);
+        }
+        private static void StartVinylAnimation(Vinyl component)
+        {
+            component.VinylAnimationGameObject.GetComponent<Animator>().enabled = true;
+        }
+
+        private void RegisterVinyl(Vinyl component)
+        {
+            StartVinylAnimation(component);
+        }
         private void ChangeRotationSpeed(VinylMusicComponent vinylMusic, Turntable component)
         {
+            if (!(IoC.Game.GameStateContext.CurrentState.Value is Running)) return;
+
             component.Speed.Value = component.MaxSpeed * vinylMusic.VinylMusicSource.pitch;
         }
 
         private void UpdateNeedlePosition(Arm component, VinylMusicComponent vinylMusicComponent)
         {
+            if (!(IoC.Game.GameStateContext.CurrentState.Value is Running)) return;
+
             var rotationDifference = component.EndRotation - component.StartRotation;
             var absoluteDifference = rotationDifference * vinylMusicComponent.MusicProgress.Value;
             var currentRotation = component.StartRotation + absoluteDifference;
