6c06f97e4c9d369ae50f5d7296b01aa5040591d8
HelenBohleber <helen.bohleber@brickmakers.de>
2020-10-05 15:01:53 +0200

    allow click on broom and hammer in ui

---
+++ Assets/Systems/Tools/BroomButton.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Systems.Tools.Actions;
+using SystemBase;
+using UniRx;
+using UnityEngine;
+
+public class BroomButton : GameComponent
+{
+    public void OnMouseDown()
+    {
+        MessageBroker.Default.Publish(
+            new SelectToolAction
+            {
+                ToolToSelect = CurrentTool.Broom
+            });
+    }
+}
