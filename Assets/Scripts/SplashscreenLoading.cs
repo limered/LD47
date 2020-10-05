025ed8dc4f3b6678d674f6eb7649fd1e413dc76b
Michael Scherbakow <ryupold@gmail.com>
2020-10-05 18:22:28 +0200

    add splashscreen with 5 second wait time before starting the game

---
+++ Assets/Scripts/SplashscreenLoading.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UniRx;
+using System;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class SplashscreenLoading : MonoBehaviour
+    {
+        public float loadTime = 5f;
+        public bool autoplay = false;
+        public GameObject progressBar;
+        public GameObject playButton;
+
+        private void Start()
+        {
+            Observable.Timer(TimeSpan.FromSeconds(loadTime))
+                .DoOnSubscribe(() =>
+                {
+                    progressBar?.SetActive(true);
+                    playButton?.SetActive(false);
+                })
+                .Subscribe(_ =>
+                {
+                    progressBar?.SetActive(autoplay || false);
+                    playButton?.SetActive(!autoplay);
+                    
+                    if (autoplay) StartGame();
+                })
+                .AddTo(this);
+        }
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene("main", LoadSceneMode.Single);
+        }
+    }
+}
