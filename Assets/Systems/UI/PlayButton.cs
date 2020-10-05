14b5e8c73c05779f4d91a9cdb721eee6522f2376
HelenBohleber <helen.bohleber@brickmakers.de>
2020-10-05 22:31:03 +0200

    reset play button sprite on game finished

--- Assets/Systems/UI/PlayButton.cs
+++ Assets/Systems/UI/PlayButton.cs
@@ -1,18 +1,29 @@
-﻿using SystemBase;
+﻿using GameState.States;
+using SystemBase;
 using UniRx;
 using UnityEngine;
+using Utils;
 
 namespace Assets.Systems.UI
 {
     public class PlayButton : GameComponent
     {
         public GameObject Play;
-        public Sprite ButtonPressedSprite;
+        public Sprite[] ButtonSprites;
 
         public void OnMouseDown()
         {
             MessageBroker.Default.Publish(new PlayButtonClickedEvent());
-            Play.GetComponent<SpriteRenderer>().sprite = ButtonPressedSprite;
+            Play.GetComponent<SpriteRenderer>().sprite = ButtonSprites[1];
+        }
+        protected override void OverwriteStart()
+        {
+            IoC.Game.GameStateContext.CurrentState.Where(state => state is StartScreen || state is GameOver).Subscribe(_ => ResetSprite());
+        }
+
+        private void ResetSprite()
+        {
+            Play.GetComponent<SpriteRenderer>().sprite = ButtonSprites[0];
         }
     }
 }
